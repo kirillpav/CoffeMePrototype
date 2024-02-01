6ccb49f94165301fa9013adce541ac4abb5955ac
kirill pavlov <2020kirillp@gmail.com>
2024-02-01 09:21:28 -0800

    Initial Commit

---
+++ CoffeMePrototype/CoffeMePrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeMePrototypeApp.swift
+//  CoffeMePrototype
+//
+//  Created by Kirill Pavlov on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeMePrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
